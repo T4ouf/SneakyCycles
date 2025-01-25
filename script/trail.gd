88a9fd506b4c1fb36f19591d307b1e87d0ed9dc4
Brice Pointal <borice.lipant@gmail.com>
2025-01-25 18:06:26 +0100

    Functioning dynamic coloring of trails and particles

--- script/trail.gd
+++ script/trail.gd
@@ -7,6 +7,7 @@ func _ready() -> void:
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
+	
 
 func _on_fading_timer_timeout() -> void:
 	hide()
